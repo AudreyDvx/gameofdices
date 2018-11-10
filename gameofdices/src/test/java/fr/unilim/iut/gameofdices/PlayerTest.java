7660871561a51c6154a42c36b17baa82e8aeae18
Audrey <audrey.devaux@etu.unilim.fr>
2018-11-10 23:06:08 +0100

    fin

--- gameofdices/src/test/java/fr/unilim/iut/gameofdices/PlayerTest.java
+++ gameofdices/src/test/java/fr/unilim/iut/gameofdices/PlayerTest.java
@@ -1,8 +1,13 @@
 package fr.unilim.iut.gameofdices;
 
-import java.util.Random;  
+import java.util.Random;   
 import org.junit.Test;
 import static org.junit.Assert.*;
+import fr.unilim.iut.gameofdices.Dice;
+import static org.mockito.Mockito.mock; 
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 public class PlayerTest {
 	Player player;
@@ -19,4 +24,27 @@ public class PlayerTest {
 		player.play();
 		assertTrue(player.getLastValue().isPresent());
 	}
+	
+	@Test
+	public void throwDiceOnlyTwice() {
+		Dice dice = mock(Dice.class);
+		player = new Player("John Doe", dice);
+		player.play();
+		verify(dice, times(2)).roll();
+	}  
+	
+	@Test
+	public void keepTheMaximum() {
+		Dice dice = mock(Dice.class);
+		player = new Player("John Doe", dice);
+
+		when(dice.roll()).thenReturn(2).thenReturn(5);
+		player.play();
+		assertEquals(player.getLastValue().get(), new Integer(5));
+
+		when(dice.roll()).thenReturn(6).thenReturn(1);
+		player.play();
+		assertEquals(player.getLastValue().get(), new Integer(6));
+	}
+	
 }   
